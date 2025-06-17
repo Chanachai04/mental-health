8ce9f1ba8c21921dc0f6d1f03540a7ba5cee1733
MIXThanakorn <s6652410021@sau.ac.th>
2025-06-17 16:24:23 +0700

    Backend & Frontend แก้ใหม่

--- backend/server.js
+++ backend/server.js
@@ -6,17 +6,14 @@ const twitterRoutes = require("./routers/twitter.route");
 const instagramRoutes = require("./routers/instagram.route");
 const { handleSearch } = require("./controllers/tiktok.controller");
 const tiktokRoutes = require("./routers/tiktok.route");
-const sentimentRoutes = require("./routers/sentiment.route");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(express.json());
 app.use(cors());
 app.use("/api/facebook", facebookRoutes);
 app.use("/api/twitter", twitterRoutes);
 app.use("/api/instagram", instagramRoutes);
 app.use("/api/tiktok", tiktokRoutes);
-app.use("/api/ai", sentimentRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
